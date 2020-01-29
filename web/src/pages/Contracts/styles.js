24e03e8a6998bff4803254e45788d3b9bea14118
Elias Gabriel <eliasgabrielcf@gmail.com>
2020-01-29 15:24:22 -0300

    Ajuste de css

--- web/src/pages/Contracts/styles.js
+++ web/src/pages/Contracts/styles.js
@@ -16,6 +16,9 @@ export const Container = styled.div`
 export const Content = styled.div`
 	max-width: 1200px;
 	width: 100%;
+
+	display: flex;
+	flex-direction: column;
 `;
 
 export const RegisterButton = styled(Button)`
